6463e5aa85811c2314163163586f588abacfe867
JasserChihi <jasserchihi1@gmail.com>
2025-06-18 23:07:10 +0100

    Create DatabaseConnection.java

---
+++ java sql/src/DatabaseConnection.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Formation";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "jasser";
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
